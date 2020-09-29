8a301393b62c1f190c25174e7206e8677e7003cc
Jady Tippetts <jady11@gmail.com>
2020-09-29 16:14:03 -0600

    character variables

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var howManyCharacters = 0;
+var specialCharacters = [!@#$%^&*()+="':;<,>.?/|\~`"]
+var lowerCaseLetter = [abcdefghijklmnopqrstuvwxwz]
+var upperCaseLetter = [ABCDEFGHHIJKLMONOPQRSTUVWXYZ]
+var numbers = [1234567890]
 
+
+<nav class="nav nav-tabs nav-stacked">
+  <a class="nav-link active" href="#">Active link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#">Disabled link</a>
+</nav>
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
